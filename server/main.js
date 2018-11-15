3bd7a6c43d7a5b43c2b618a7ecdfa14809787316
Ayoola Ajebeku <ayz4sci@yahoo.com>
2018-11-15 19:46:20 +0100

    Refactors folder structure

--- server/main.js
+++ server/main.js
@@ -5,10 +5,11 @@ const app = new express();
 const port = process.env.PORT || 7777;
 
 app.get('/', (req, res) => {
-    res.render('./../app/index.html', {})
+    res.sendFile('index.html', {root : __dirname + './../public'})
 })
-.use(express.static(__dirname +'./../app/'))
-.listen(port, () => console.log(`Listening on port ${port}`))
+.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended:false}));
